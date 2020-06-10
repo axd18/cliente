9220e7a27037be77b26fdd3aefb3add697b76e85
Adrian <adrianfernandezj@gmail.com>
2020-06-10 07:03:05 -0300

    190-state y funciones para el componente

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
 
 const Login = () => {
     
-    const onChange = () => {
-
+    const onChange = e => {
+        guardarUsuario({
+            ...usuario,
+            [e.target.name] : e.target.value
+        });
     }
+
+    // Cuando el usuario quiere iniciar sesion
+    const onSubmit = e => {
+        e.preventDefault();
+
+        // Validar que no haya campos vacíos
+        
+        // Pasarlo al action
+    }
+    
+    // State para iniciar sesión
+    const [usuario, guardarUsuario] = useState({
+        email: '',
+        password:''
+    });
+
+    // Extraer de usuario
+    const {email, password} = usuario;
     
     
     return ( 
@@ -12,7 +34,9 @@ const Login = () => {
             <div className="contenedor-form sombra-dark">
                 <h1>Iniciar Sesión</h1>
 
-                <form>
+                <form
+                    onSubmit={onSubmit}
+                >
                     <div className="campo-form">
                         <label htmlFor="email">Email</label>
                         <input
@@ -20,6 +44,7 @@ const Login = () => {
                             id="email"
                             name="email"
                             placeholder="Tu Email"
+                            value={email}
                             onChange={onChange}
                         />
                     </div>
@@ -30,6 +55,7 @@ const Login = () => {
                             id="password"
                             name="password"
                             placeholder="Tu Password"
+                            value={password}
                             onChange={onChange}
                         />
                     </div>
@@ -37,6 +63,9 @@ const Login = () => {
                         <input type="submit" className="btn btn-primario btn-block" value="Iniciar Sesión"/>   
                     </div>
                 </form>
+                <Link to={'/nueva-cuenta'} className="enlace-cuenta">
+                    Obtener Cuenta
+                </Link>
             </div>  
         </div>
      );
--- src/components/auth/NuevaCuenta.js
+++ src/components/auth/NuevaCuenta.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const NUevaCuenta = () => {
+const NuevaCuenta = () => {
     return ( 
         <h1>desde Nueva Cuenta</h1>
      );
 }
  
-export default NUevaCuenta;
+export default NuevaCuenta;
