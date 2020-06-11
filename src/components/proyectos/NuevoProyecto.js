8e0f9f296b205505dc0a0c0299ffbbc02d097be1
adrian <adrianfernandezj@gmail.com>
2020-06-11 19:18:05 -0300

    194-State y funciones para nuevos proyectos / 195-Listando los Proyectos Creados

--- src/components/proyectos/NuevoProyecto.js
+++ src/components/proyectos/NuevoProyecto.js
@@ -1,6 +1,37 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 const NuevoProyecto = () => {
+    
+    // State para Proyecto
+    const [proyecto, guardarProyecto] = useState({
+        nombre:''
+    });
+
+    // Extraer nombre de proyecto
+    const { nombre } = proyecto;
+
+    
+    // Lee los comentarios del input
+    const onChangeProyecto = e => {
+        guardarProyecto({
+            ...proyecto,
+            [e.target.name] : e.target.value
+        })
+    }
+    
+    // Cuando el usuario envía un proyecto
+    const onSubmitProyecto = e => {
+        e.prventDefault();
+
+        // validar el proyecto
+
+
+        // agregar al state
+
+
+        // Reiniciar el form
+    }
+    
     return ( 
         <Fragment>
             <button
@@ -8,12 +39,17 @@ const NuevoProyecto = () => {
                 className="btn btn-block btn-primario"
             >NuevoProyecto</button>
             
-            <form>
+            <form
+                className="formulario-nuevo-proyecto"
+                onSubmit={onSubmitProyecto}
+            >
                 <input
                     type="text"
                     className="input-text"
                     placeholder="Nombre del Proyecto"
                     name="nombre"
+                    value={nombre}
+                    onChange={onChangeProyecto}
                 />
                 <input
                     type="submit"
