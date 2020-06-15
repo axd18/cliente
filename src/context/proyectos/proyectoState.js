0bddb0edf9a9d905896f05dd9036e0c3cce4fff7
adrian <adrianfernandezj@gmail.com>
2020-06-15 16:41:28 -0300

    207-Colocar un proyecto en activo

--- src/context/proyectos/proyectoState.js
+++ src/context/proyectos/proyectoState.js
@@ -8,7 +8,8 @@ import {
     FORMULARIO_PROYECTO,
     OBTENER_PROYECTOS,
     AGREGAR_PROYECTO,
-    VALIDAR_FORMULARIO
+    VALIDAR_FORMULARIO,
+    PROYECTO_ACTUAL
 } from '../../types';
 
 
@@ -27,7 +28,8 @@ const ProyectoState = props => {
     const initialState = {
         proyectos : [],
         formulario : false,
-        errorformulario: false
+        errorformulario: false,
+            proyecto: null
     }
 
     //Dispatch para ejecutar las acciones
@@ -69,16 +71,26 @@ const ProyectoState = props => {
         })
     }
 
+    // Selecciona el proyecto que el usuario dio click
+    const proyectoActual = proyectoId => {
+        dispatch({
+            type: PROYECTO_ACTUAL,
+            payload: proyectoId
+        })
+    }
+
     return (
         <proyectoContext.Provider
             value={{
                 proyectos: state.proyectos,
                 formulario: state.formulario,
+                proyecto: state.proyecto,
                 mostrarFormulario,
                 errorformulario: state.errorformulario,
                 obtenerProyectos,
                 agregarProyecto,
-                mostrarError
+                mostrarError,
+                proyectoActual
             }}
         >
             {props.children}
