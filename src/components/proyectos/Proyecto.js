785a85245dff5075835f9f681ca6f0898d0ad955
adrian <adrianfernandezj@gmail.com>
2020-06-16 18:50:12 -0300

    213-Creando una funcion que obtenga las tareas del proyecto

--- src/components/proyectos/Proyecto.js
+++ src/components/proyectos/Proyecto.js
@@ -1,20 +1,28 @@
 import React, {useContext} from 'react';
 import proyectoContext from '../../context/proyectos/proyectoContext';
+import tareaContext from '../../context/tareas/tareaContext';
 
 const Proyecto = ({proyecto}) => {
     
     // Obtener el state de proyectos
     const proyectosContext = useContext(proyectoContext);
     const { proyectoActual } = proyectosContext;
+
+    // obtener la funcion dell context de tarea
+    const tareasContext = useContext(tareaContext);
+    const {obtenerTareas} = tareasContext;
     
-    
-    
+    // Función para agregar el proyecto actual
+    const seleccionarProyecto = id => {
+        proyectoActual(id); // Fijar un proyecto actual
+        obtenerTareas(id); // Filtrar las tareas cuando se de click
+    }
     return ( 
         <li>
             <button
                 type="button"
                 className="btn btn-blank"
-                onClick={() => proyectoActual(proyecto.id)}
+                onClick={() => seleccionarProyecto(proyecto.id) }
             >{proyecto.nombre}</button> 
         </li>
      );
