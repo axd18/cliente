37c7ce9899d9b0442dfe5308056314f66a424ddb
adrian <adrianfernandezj@gmail.com>
2020-06-20 13:46:58 -0300

    219-Agregando una animacion cuando se crean nuevas tareas

--- src/context/tareas/tareaReducer.js
+++ src/context/tareas/tareaReducer.js
@@ -19,7 +19,7 @@ export default (state, action) => {
         case AGREGAR_TAREA:
             return {
                 ...state,
-                tareas: [...state.tareas, action.payload],
+                tareas: [action.payload, ...state.tareas],
                 errortarea: false
             }
 
