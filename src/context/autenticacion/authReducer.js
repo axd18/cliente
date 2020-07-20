089baaa6af4f374811c47316afcfa898029d6467
adrian <adrianfernandezj@gmail.com>
2020-07-20 18:06:58 -0300

    261-Creando el state y el provider

---
+++ src/context/autenticacion/authReducer.js
@@ -0,0 +1,18 @@
+import {
+    REGISTRO_EXITOSO,
+    REGISTRO_ERROR,
+    OBTENER_USUARIO,
+    LOGIN_EXITOSO,
+    LOGIN_ERROR,
+    CERRAR_SESION
+} from '../../types';
+
+
+export default (state, action) => {
+    switch(action.type) {
+        
+        
+        default:
+            return state;
+    }
+}
