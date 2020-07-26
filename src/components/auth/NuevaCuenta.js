2a7f9b1e57ed966e75e48f3e03b2bbc291eacddb
adrian <adrianfernandezj@gmail.com>
2020-07-26 00:47:43 -0300

    263-Leyendo datos del formulario de React y enviandolos a la API

--- src/components/auth/NuevaCuenta.js
+++ src/components/auth/NuevaCuenta.js
@@ -1,66 +1,85 @@
 import React, { useState, useContext, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import AlertaContext from '../../context/alertas/alertaContext';
+import AuthContext from '../../context/autenticacion/authContext';
 
-const NuevaCuenta = () => {
-    
-    // Extraer los valores del context
+const NuevaCuenta = (props) => {
+
+    // extraer los valores del context
     const alertaContext = useContext(AlertaContext);
     const { alerta, mostrarAlerta } = alertaContext;
 
+    const authContext = useContext(AuthContext);
+    const { mensaje, autenticado,  registrarUsuario } = authContext;
+
+    // En caso de que el usuario se haya autenticado o registrado o sea un registro duplicado
+    useEffect(() => {
+        if(autenticado) {
+            props.history.push('/proyectos');
+        }
+
+        if(mensaje) {
+            mostrarAlerta(mensaje.msg, mensaje.categoria);
+        }
+        // eslint-disable-next-line
+    }, [mensaje, autenticado, props.history]);
+
     // State para iniciar sesión
     const [usuario, guardarUsuario] = useState({
-        nombre:'',
+        nombre: '',
         email: '',
-        password:'',
+        password: '',
         confirmar: ''
     });
-    
-    // Extraer de usuario
-    const {nombre, email, password, confirmar} = usuario;
-    
-    
+
+    // extraer de usuario
+    const { nombre, email, password, confirmar } = usuario;
+
     const onChange = e => {
         guardarUsuario({
             ...usuario,
             [e.target.name] : e.target.value
-        });
+        })
     }
 
-    // Cuando el usuario quiere iniciar sesion
+    // Cuando el usuario quiere iniciar sesión
     const onSubmit = e => {
         e.preventDefault();
 
-        // Validar que no haya campos vacíos
-        if( nombre.trim() === '' ||
-            email.trim() === '' ||
-            password.trim() === '' ||
-            confirmar.trim() === '') {
+        // Validar que no haya campos vacios
+        if( nombre.trim() === '' || 
+            email.trim() === '' || 
+            password.trim() === '' || 
+            confirmar.trim() === '' ) {
                 mostrarAlerta('Todos los campos son obligatorios', 'alerta-error');
                 return;
             }
 
         // Password minimo de 6 caracteres
         if(password.length < 6) {
-           mostrarAlerta('El campo debe ser al menos de 6 caracteres','alerta-error');
-           return;
+            mostrarAlerta('El password debe ser de al menos 6 caracteres', 'alerta-error');
+            return;
         }
 
         // Los 2 passwords son iguales
         if(password !== confirmar) {
-            mostrarAlerta('Los passwords no son iguales','alerta-error');
-            return; 
+            mostrarAlerta('Los passwords no son iguales', 'alerta-error');
+            return;
         }
-        
-        // Pasarlo al action
-    }
-    
 
-    
-    
+        // Pasarlo al action
+        registrarUsuario({
+            nombre, 
+            email, 
+            password
+        });
+    }
+
+
+
     return ( 
         <div className="form-usuario">
-            {alerta ? (<div className={`alerta${alerta.categoria}`}> {alerta.msg} </div>) : null}
+            { alerta ? ( <div className={`alerta ${alerta.categoria}`}> {alerta.msg} </div> )  : null }
             <div className="contenedor-form sombra-dark">
                 <h1>Obtener una cuenta</h1>
 
@@ -69,7 +88,7 @@ const NuevaCuenta = () => {
                 >
                     <div className="campo-form">
                         <label htmlFor="nombre">Nombre</label>
-                        <input
+                        <input 
                             type="text"
                             id="nombre"
                             name="nombre"
@@ -78,9 +97,10 @@ const NuevaCuenta = () => {
                             onChange={onChange}
                         />
                     </div>
+
                     <div className="campo-form">
                         <label htmlFor="email">Email</label>
-                        <input
+                        <input 
                             type="email"
                             id="email"
                             name="email"
@@ -89,9 +109,10 @@ const NuevaCuenta = () => {
                             onChange={onChange}
                         />
                     </div>
+
                     <div className="campo-form">
                         <label htmlFor="password">Password</label>
-                        <input
+                        <input 
                             type="password"
                             id="password"
                             name="password"
@@ -100,25 +121,28 @@ const NuevaCuenta = () => {
                             onChange={onChange}
                         />
                     </div>
+
                     <div className="campo-form">
-                        <label htmlFor="confirmar">confirmar Password</label>
-                        <input
+                        <label htmlFor="confirmar">Confirmar Password</label>
+                        <input 
                             type="password"
                             id="confirmar"
                             name="confirmar"
-                            placeholder="Repite tu password"
+                            placeholder="Repite tu Password"
                             value={confirmar}
                             onChange={onChange}
                         />
                     </div>
+
                     <div className="campo-form">
-                        <input type="submit" className="btn btn-primario btn-block" value="Registrarme"/>   
+                        <input type="submit" className="btn btn-primario btn-block" value="Registrarme" />
                     </div>
                 </form>
+
                 <Link to={'/'} className="enlace-cuenta">
                     Volver a Iniciar Sesión
                 </Link>
-            </div>  
+            </div>
         </div>
      );
 }
