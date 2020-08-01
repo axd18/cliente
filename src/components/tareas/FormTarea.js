e16b07d9f48fc156fe3e57f3a8d90228d5dc95dd
axd18 <adrianfernandezj@hotmail.com>
2020-08-01 16:50:34 -0300

    216-Insertando tareas en el proyecto actual

--- src/components/tareas/FormTarea.js
+++ src/components/tareas/FormTarea.js
@@ -1,5 +1,6 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState } from 'react';
 import proyectoContext from '../../context/proyectos/proyectoContext';
+import tareaContext from '../../context/tareas/tareaContext';
 
 const FormTarea = () => {
     
@@ -7,21 +8,44 @@ const FormTarea = () => {
     const proyectosContext = useContext(proyectoContext);
     const { proyecto } = proyectosContext;
 
+    // obtener la funcion del context de tarea
+    const tareasContext = useContext(tareaContext);
+    const {agregarTarea} = tareasContext;
+
+    // State del formulario
+    const [ tarea, guardarTarea ] = useState({
+        nombre:''
+    })
+
+    // extraer nombre del proyecto
+    const { nombre } = tarea;
+
+
     // Si no hay proyecto seleccionado
     if(!proyecto) return null;
 
     
     // Array destructuring para extraer el proyecto actual
     const [proyectoActual] = proyecto;
+
+    // Leer los valores del formulario
+    const handleChange = e => {
+        guardarTarea({
+            ...tarea,
+            [e.target.name] : e.target.value
+        })
+    }
     
     const onSubmit = e => {
         e.preventDefault();
 
         //validar
-
         //pasar la validacion
 
-        //agregar la nueva tarea
+        //agregar la nueva tarea al state de tareas
+        tarea.proyectoId = proyectoActual.id;
+        tarea.estado = false;
+        agregarTarea(tarea);
 
         //reiniicar el form
 
@@ -38,6 +62,8 @@ const FormTarea = () => {
                         className="input-text"
                         placeholder="Nombre Tarea..."
                         name="nombre"
+                        value={nombre}
+                        onChange={handleChange}
                     />
                 </div>
                 <div className="contenedor-input">
